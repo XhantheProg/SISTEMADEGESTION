03801ef6e0a87926a5d7c54760af4bc9c370bf0b
XhantheProg <Curlydoge107@gmail.com>
2025-12-18 23:01:59 -0400

    Agregar elementos HTML y mejorar la estructura del script

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("llegaste hasta aqui");
+const encabezado3 = document.getElementsByTagName('h3');
+console.log(encabezado3);
+
+const encabezado2 = document.getElementsByTagName('h2');
+console.log(encabezado2);
+
+const caja1 = document.getElementsByClassName('caja1');
+console.log(caja1); 
