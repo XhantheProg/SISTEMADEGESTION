9bb67852cf00c3c782fbd049c6ee7fad914ff5e0
XhantheProg <Curlydoge107@gmail.com>
2025-12-20 11:47:44 -0400

    Agregar navegación y páginas de inicio y sobre al proyecto

---
+++ proyrouter/js/pages/about.js
@@ -0,0 +1,6 @@
+function renderAboutPage(root) {
+    root.innerHTML = ` 
+    <h1>About Page</h1>
+    <p>This is the about page of our application.</p>
+    `;
+}
---
+++ proyrouter/js/pages/home.js
@@ -0,0 +1,6 @@
+function renderHomePage(root) {
+    root.innerHTML = ` 
+    <h1>Inicio</h1>
+    <p>Bienvenido a la página de inicio de nuestra aplicación.</p>`;
+    
+}
